32723ac6e8008d5a0ca49b899b83d9d43701df30
Alberto Liguori <a1liguorialberto@gmail.com>
2020-11-02 14:46:16 +0100

    Open and close Hambuger menu

---
+++ main.js
@@ -0,0 +1,9 @@
+//APRE HAMBURGER MENU
+$(".header-right > a").click(function(){
+  $(".hamburger-menu").show()
+})
+
+//CHIUDE HAMBURGER MENU
+$(".hamburger-menu > .close").click(function(){
+  $(".hamburger-menu").hide()
+})
