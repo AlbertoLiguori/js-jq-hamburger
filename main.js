ae23be17bb786060b4efb612d3531e39eab58508
Alberto Liguori <a1liguorialberto@gmail.com>
2020-11-02 15:35:40 +0100

    Modified as suggested

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //APRE HAMBURGER MENU
 $(".header-right > a").click(function(){
-  $(".hamburger-menu").show()
+  $(".hamburger-menu").addClass("active")
 })
 
 //CHIUDE HAMBURGER MENU
 $(".hamburger-menu > .close").click(function(){
-  $(".hamburger-menu").hide()
+  $(".hamburger-menu").removeClass("active")
 })
